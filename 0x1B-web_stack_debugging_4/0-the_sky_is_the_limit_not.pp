262df2b124cb53fb90e1f692f94757d98995632e
ebhenhaazer <en.mwathy@gmail.com>
2024-01-25 06:11:20 -0800

    corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
 # The limit of requests is changed in Nginx default file
+
 exec { 'increase the uLimit of nginx':
   command => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
   path    => ['/bin']
@@ -8,3 +9,4 @@ exec { 'restart nginx':
   command => 'service nginx restart',
   path    => ['/usr/bin']
 }
+
